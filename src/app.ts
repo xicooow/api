b32ecde7c614c8ec71078ed6d1d5ce6d4f4eb91d
Francisco Neto <xico.dova@gmail.com>
2022-09-19 21:59:15 -0300

    Init

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import express from "express";
+
+/** load .env stuff */
+dotenv.config();
+
+import dbConnect from "./db/connect";
+import { APP_PORT } from "./constants";
+
+const app = express();
+
+/** middlewares */
+app.use(express.json());
+
+/** api server start */
+app.listen(APP_PORT, async () => {
+  console.log(`App listening in port ${APP_PORT}`);
+  await dbConnect();
+});
---
+++ src/db/connect.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+import { APP_DB_URL } from "../constants";
+import { getErrorMessage } from "../helpers/util";
+
+export default async () => {
+  try {
+    await mongoose.connect(`${APP_DB_URL}/core`);
+    console.log("Successfully connected to MongoDB");
+  } catch (error) {
+    throw new Error(`Failed to connect to MongoDB: ${getErrorMessage(error)}`);
+  }
+};
